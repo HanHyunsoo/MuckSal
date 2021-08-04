02564152e9ad6dae6c1a1b73c6ce6f148db763b7
wa1569 <vkxm0826@naver.com>
2021-08-04 15:59:27 +0900

    08.04

--- recipe/urls.py
+++ recipe/urls.py
@@ -4,4 +4,8 @@ import recipe.views
 app_name = "recipe"
 urlpatterns = [
     path('explain/', recipe.views.explain, name="explain"),
+    path('search/', recipe.views.search, name = "serach"),
+    path('add/', recipe.views.add, name = "add"),
+    path('list/', recipe.views.list, name = "list"),
+    path('add/create/', recipe.views.create, name = "create"),
 ]
