6c3a3b00a024725bbd82ad760eadfd43928ee680
김예지 <yjiu9992@gmail.com>
2021-08-12 06:17:01 +0900

    fix account  admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+class AccountsAdmin(admin.ModelAdmin):
+    # pass 이거 사용하면 상속만 받아서 새로운 클래스 생성
+    list_display = ('username','password')
+
+admin.site.register(User,AccountsAdmin)
