cae21895af47bfe32957b5d95a68b38e30964575
김예지 <yjiu9992@gmail.com>
2021-08-12 15:24:57 +0900

    :wrench: fix the models

--- accounts/models.py
+++ accounts/models.py
@@ -7,7 +7,7 @@ class User(AbstractUser):
     username = models.CharField(max_length=32, unique=True)
     password=models.CharField(max_length=64)
     phone_number = models.CharField(max_length=20)
-    address = models.CharField(max_length=200)
+    useremail = models.EmailField(max_length=128)
     is_premium = models.BooleanField(default=False)
     register_dttm = models.DateField(auto_now=True, verbose_name="가입날짜")
     def __str__(self): #데이터가 문자열 변환될 때 어떻게 나오는 지 보려고
