0f57404588c78dfcae62275b993b3fedc7b6d354
wa1569 <vkxm0826@naver.com>
2021-11-29 19:06:18 +0900

    최종 완성

--- recipe/forms.py
+++ recipe/forms.py
@@ -1,12 +1,30 @@
 from django import forms
 from django.db.models import fields
+from django.forms.widgets import TextInput, Textarea, Widget
 from .models import Recipe
 from django import forms
 
 class CreateRecipe(forms.ModelForm):
     class Meta:
         model = Recipe
-        fields = ['title', 'body']
+        fields = ['title', 'body', 'ingredient']
+        widgets ={
+            'title' : TextInput(attrs={
+                'class' : "form-control",
+                'style' : 'max-width : 500px; text-align : center; margin : 50px;',
+                'placeholder': '음식 이름'
+            }),
+            'body': Textarea(attrs={
+                'class': "form-control",
+                'style': 'max-width: 500px; text-align : center;margin : 50px;',
+                'placeholder': '조리법'
+            }),
+            'ingredient': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 500px; text-align : center;margin : 50px;',
+                'placeholder': '재료'
+            }),
+        }
 
 class PostSearchForm(forms.Form):
     search_word = forms.CharField(label='Search Word')
--- recipe/models.py
+++ recipe/models.py
@@ -1,13 +1,21 @@
 from django.db import models
 from django.conf import settings
+from django.db.models.fields import related
 
 # Create your models here.
 
 class Recipe(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
+    ingredient = models.TextField(null=True)
+    hits = models.PositiveIntegerField(default=0)
     # food = models.OneToOneField("diet.Food", on_delete=models.CASCADE)
 
 
     def __srt__(self):
         return self.title
+
+    @property
+    def click(self):
+        self.hits += 1
+        self.save()
